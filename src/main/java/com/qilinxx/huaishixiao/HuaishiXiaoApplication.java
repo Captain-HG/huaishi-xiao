b8a47b7013c4c86db3f6004463715825c22594f2
lzc <1436899024@qq.com>
2019-01-03 09:25:57 +0800

    Initial commit

---
+++ src/main/java/com/qilinxx/huaishixiao/HuaishiXiaoApplication.java
@@ -0,0 +1,14 @@
+package com.qilinxx.huaishixiao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuaishiXiaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuaishiXiaoApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/qilinxx/huaishixiao/HuaishiXiaoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.qilinxx.huaishixiao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HuaishiXiaoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
